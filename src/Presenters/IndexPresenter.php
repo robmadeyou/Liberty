fafe1b96d75f3057160b648e81971c88b8bf7acf
robmadeyou <robmadeyou@gmail.com>
2015-11-27 08:58:40 +0000

    dfgbf

--- src/Presenters/IndexPresenter.php
+++ src/Presenters/IndexPresenter.php
@@ -10,4 +10,83 @@ class IndexPresenter extends HtmlPresenter
     {
         return new IndexView();
     }
+
+    public static function checkIfNameIsTaken( $name )
+    {
+        if( MySql::returnSingleValue( "SELECT COUNT( ContactID ) FROM tblContact WHERE tblContact.Name = :contact", [ 'contact' => $name ] ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkIfEmailIsTaken( $email )
+    {
+        if( MySql::returnSingleValue( "SELECT COUNT( ContactID ) FROM tblContact WHERE tblContact.ContactEmail = :email", [ 'email' => $email ] ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkIfWebsiteIsTaken( $website )
+    {
+        if( MySql::returnSingleValue( "SELECT COUNT( ContactID ) FROM tblContact WHERE tblContact.Website = :website", [ 'website' => $website ] ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkIfCompanyIsTaken( $company )
+    {
+        if( MySql::returnSingleValue( "SELECT COUNT( ContactID ) FROM tblContact WHERE tblContact.CompanyName = :company", [ 'company' => $company ] ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    protected function configureView()
+    {
+        $this->view->attachEventHandler( 'InputCheck', function( $name, $value )
+        {
+            switch( $name )
+            {
+                case 'Name':
+                    if( self::checkIfNameIsTaken( $value ) )
+                    {
+                        return 0;
+                    }
+                    return 3;
+                case 'Email':
+                    if( preg_match( '/.+@.+\..+/', $value ) === 1 )
+                    {
+                        if( self::checkIfEmailIsTaken( $value ) )
+                        {
+                            return 0;
+                        }
+                        return 3;
+                    }
+                    else
+                    {
+                        return 3;
+                    }
+                case 'Website':
+                    if( self::checkIfWebsiteIsTaken( $value ) )
+                    {
+                        return 0;
+                    }
+                    return 3;
+                case 'Company':
+                    if( self::checkIfCompanyIsTaken( $value ) )
+                    {
+                        return 0;
+                    }
+                    return 3;
+            }
+            return 2;
+        });
+        return parent::configureView();
+    }
 }
